e78b5371d7c238745b9ea4a4f4f6ce69a69eeb77
xushuhui <474497097@qq.com>
2021-11-11 15:15:39 +0800

    feat: codemapping

--- core/message.go
+++ core/message.go
@@ -2,23 +2,25 @@ package core
 
 const (
 	SUCCESS = iota
-	InvalidParams
-	DBError
 	ServerError
+	DBError
+
+	InvalidParams
 )
 
-var MsgFlags = map[int]string{
-	SUCCESS:       "ok",
+var CodeMapping = map[int]string{
+	SUCCESS:     "ok",
+	ServerError: "系统异常，请联系管理员！",
+	DBError:     "数据库错误",
+
 	InvalidParams: "请求参数错误",
-	DBError:       "数据库错误",
-	ServerError:   "系统异常，请联系管理员！",
 }
 
 func GetMsg(code int) string {
-	msg, ok := MsgFlags[code]
+	msg, ok := CodeMapping[code]
 	if ok {
 		return msg
 	}
 
-	return MsgFlags[InvalidParams]
+	return CodeMapping[InvalidParams]
 }
