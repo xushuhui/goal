be195bcac345e9883d52cdc51e9175c170254bd8
xushuhui <474497097@qq.com>
2023-11-03 10:28:47 +0800

    fix

--- config/config.go
+++ config/config.go
@@ -5,7 +5,7 @@ var (
 	WireCmd       = "github.com/google/wire/cmd/wire@latest"
 	GoalCmd       = "github.com/xushuhui/goal@latest"
 	RepoBase      = "https://github.com/xushuhui/goal-layout-base.git"
-	RepoAdvanced  = "https://github.com/xushuhui/goal-layout-advanced.git"
+	RepoAdvanced  = "https://github.com/xushuhui/goal-advanced-layout.git"
 	RunExcludeDir = ".git,.idea,tmp,vendor"
 	RunIncludeExt = "go,html,yaml,yml,toml,ini,json,xml,tpl,tmpl"
 )
